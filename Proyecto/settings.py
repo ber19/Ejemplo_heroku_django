df1b079cc56e94730f924fc9e9d5f65efe419427
BerFlores <berflores19@gmail.com>
2021-01-16 14:54:17 -0600

    integracion con S3

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -140,3 +141,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = 'Plataforma/'
 
 LOGIN_URL = 'login'
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('S3_BUCKET')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
